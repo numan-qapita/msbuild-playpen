ab8b0905606666cb34caea7119282ad6b966ea4a
Numan Mohammed <numan@qapita.com>
2023-04-02 22:23:55 +0530

    added sample code to demonstrate directory.packages.props

---
+++ src/Application/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Application;
+
+public class Class1
+{
+    private readonly ILogger<Class1> _logger;
+
+    public Class1(ILogger<Class1> logger)
+    {
+        _logger = logger;
+    }
+}
